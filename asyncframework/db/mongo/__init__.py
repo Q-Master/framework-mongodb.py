7507d024ef25b2525616a2433542ce06db1b7e4a
Vladimir Berezenko <qmaster2000@gmail.com>
2023-03-27 17:41:34 +0300

    Fix for newer build system

--- asyncframework/db/mongo/__init__.py
+++ asyncframework/db/mongo/__init__.py
@@ -3,3 +3,17 @@ from .collection_field import *
 from .connection import *
 from .collection import *
 from .database import *
+
+__version__ = '0.1.1'
+
+__title__ = 'asyncframework-mongodb'
+__description__ = 'Async framework mongodb addon.'
+__url__ = 'https://github.com/Q-Master/framework-mongodb.py'
+__uri__ = __url__
+__doc__ = f"{__description__} <{__uri__}>"
+
+__author__ = 'Vladimir Berezenko'
+__email__ = 'qmaster2000@gmail.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019-2023 Vladimir Berezenko'
